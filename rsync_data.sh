a1ce0a8d78c3f6dee53908d1fc0965a58540bb88
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-07-14 11:19:01 +0200

    use 4 space for indent

--- rsync_data.sh
+++ rsync_data.sh
@@ -4,7 +4,7 @@ DATA_DIR=/mnt/data
 BACKUP_DIR=/mnt/backup
 
 if test -d $DATA_DIR && test -d $BACKUP_DIR && test -w $BACKUP_DIR; then
-	rsync -av --delete --exclude=mysql/ $DATA_DIR $BACKUP_DIR
+    rsync -av --delete --exclude=mysql/ $DATA_DIR $BACKUP_DIR
 else
-   echo "Source or backup dir don't exist, or data dir is not writeable."
+    echo "Source or backup dir don't exist, or data dir is not writeable."
 fi
