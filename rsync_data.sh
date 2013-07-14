024195c04594997f440de63b5bf8b369e334684d
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-07-14 19:42:49 +0200

    make exclude a parameter

--- rsync_data.sh
+++ rsync_data.sh
@@ -2,9 +2,11 @@
 
 DATA_DIR=/mnt/data
 BACKUP_DIR=/mnt/backup
+#EXCLUDE="--exclude=folder"
+EXCLUDE=
 
 if test -d $DATA_DIR && test -d $BACKUP_DIR && test -w $BACKUP_DIR; then
-    rsync -av --delete --exclude=mysql/ $DATA_DIR $BACKUP_DIR
+    rsync -av --delete $EXCLUDE $DATA_DIR $BACKUP_DIR
 else
     echo "Source or backup dir don't exist, or data dir is not writeable."
 fi
