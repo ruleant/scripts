fa42c88f98bff36e5e4037c294621308173939ee
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-07-15 15:24:06 +0200

    use exclude file

--- rsync_data.sh
+++ rsync_data.sh
@@ -7,9 +7,10 @@ DATA_DIR=/mnt/data
 BACKUP_DIR=/mnt/backup
 #EXCLUDE="--exclude=folder"
 EXCLUDE=
+EXCLUDEFILE=./exclude
 
 if test -d $DATA_DIR && test -d $BACKUP_DIR && test -w $BACKUP_DIR; then
-    rsync -av --delete $EXCLUDE $DATA_DIR $BACKUP_DIR
+    rsync -av --delete $EXCLUDE --exclude-from=$EXCLUDEFILE $DATA_DIR $BACKUP_DIR
 else
     echo "Source or backup dir don't exist, or data dir is not writeable."
 fi
